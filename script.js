4c95d08ae604d62da23d073e95b9df344f814840
Nathan Andrews <nandrews.programming@gmail.com>
2021-01-14 15:03:14 -0500

    Added completed and delete buttons
    Added functionality such that a completed and delete button appear next to each new list item appended to note-list

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
+let listLength = 0;
+
 document.getElementById('save-button').addEventListener('click', (event) => {
 
     const list = document.getElementById("note-list");
     
     let listItem = document.createElement('li');
+    listItem.id = 'li'+listLength;
     
+    //Creating noteDiv
     let noteText = document.getElementById('textarea').value;
     let noteDiv = document.createElement('div');
-    noteDiv.id = 'noteDiv';
+    noteDiv.className = 'noteDiv';
     noteDiv.innerHTML = noteText;
 
+    //Creating dateDiv
     let dateDiv = document.createElement('div');
-    dateDiv.id = 'date-div';
+    dateDiv.className = 'date-div';
     let date = new Date;
     let dateString = date.toDateString();
     dateDiv.innerHTML = dateString;
     
-    listItem.append(noteDiv,dateDiv);
+    //Creating completedDiv
+    let completedDiv = document.createElement('div');
+    completedDiv.className = 'completed-div';
+    let completedButton = document.createElement('button');
+    completedButton.innerHTML = 'Mark Complete';
+    completedDiv.append(completedButton);
 
+    //Creating deleteDiv
+    let deleteDiv = document.createElement('div');
+    deleteDiv.className = 'delete-div';
+    let deleteButton = document.createElement('button');
+    deleteButton.innerHTML = 'Delete';
+    deleteDiv.append(deleteButton);
+    
+    //Appending Items
+    listItem.append(noteDiv,dateDiv,completedDiv,deleteDiv);
     list.append(listItem);
 });
+
+document.getElementById
